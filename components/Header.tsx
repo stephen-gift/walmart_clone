548e06cdcc1335a15f2a9638c1bea25311a3501b
Stephen <79639335+stephen-gift@users.noreply.github.com>
2024-02-12 14:57:49 +0100

    Update

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,5 +1,32 @@
+import Link from "next/link";
+import { Button } from "./ui/button";
+import Image from "next/image";
+import { Search } from "lucide-react";
+
 function Header() {
-  return <div>Header</div>;
+  return (
+    <header>
+      <Link href={"/"} className="">
+        <Image
+          src={"https://links.papareact.com/xsi"}
+          alt="logo"
+          width={150}
+          height={150}
+        />
+      </Link>
+      <form className="flex items-center bg-white  rounded-full w-full">
+        <input
+          type="text"
+          placeholder="Search Everything..."
+          className="flex-1"
+        />
+        <button>
+          <Search className="rounded-full h-10" />
+        </button>
+      </form>
+      Header
+    </header>
+  );
 }
 
 export default Header;
--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    dangerouslyAllowSVG: true,
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "**.papareact.com",
+        port: "",
+      },
+    ],
+  },
+};
 
 export default nextConfig;
