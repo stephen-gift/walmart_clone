f2458f354349b91b08636db66e0c5bbc1d8b5377
Stephen <79639335+stephen-gift@users.noreply.github.com>
2024-02-12 15:27:41 +0100

    Update Header

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,11 +1,18 @@
 import Link from "next/link";
 import { Button } from "./ui/button";
 import Image from "next/image";
-import { Search } from "lucide-react";
+import {
+  Grid2X2,
+  Heart,
+  LayoutGrid,
+  Search,
+  ShoppingCart,
+  User,
+} from "lucide-react";
 
 function Header() {
   return (
-    <header>
+    <header className="flex flex-col md:flex-row bg-walmart px-10 py-7 items-center space-x-5">
       <Link href={"/"} className="">
         <Image
           src={"https://links.papareact.com/xsi"}
@@ -18,13 +25,59 @@ function Header() {
         <input
           type="text"
           placeholder="Search Everything..."
-          className="flex-1"
+          className="flex-1 px-4 rounded-l-full outline-none placeholder:text-sm"
         />
-        <button>
-          <Search className="rounded-full h-10" />
+        <button type="submit">
+          <Search className="rounded-full h-10 px-2 w-10 bg-yellow-400" />
         </button>
       </form>
-      Header
+
+      <div className="flex space-x-5 mt-5 md:mt-0p-5 pb-0 md:p-0">
+        <Link
+          href={"/"}
+          className="hidden xl:flex text-white font-bold items-center space-x-2 text-sm"
+        >
+          <Grid2X2 size={20} />
+          <p>Departments</p>
+        </Link>
+        <Link
+          href={"/"}
+          className="hidden xl:flex text-white font-bold items-center space-x-2 text-sm"
+        >
+          <LayoutGrid size={20} />
+          <p>Services</p>
+        </Link>
+        <Link
+          href={"/"}
+          className="flex text-white font-bold items-center space-x-2 text-sm"
+        >
+          <Heart size={20} />
+          <div>
+            <p className="text-xs font-extralight">Reorder</p>
+            <p>My Items</p>
+          </div>
+        </Link>
+        <Link
+          href={"/"}
+          className="flex text-white font-bold items-center space-x-2 text-sm"
+        >
+          <User size={20} />
+          <div>
+            <p className="text-xs font-extralight">Sigh In</p>
+            <p>Account</p>
+          </div>
+        </Link>
+        <Link
+          href={"/"}
+          className="flex text-white font-bold items-center space-x-2 text-sm"
+        >
+          <ShoppingCart size={20} />
+          <div>
+            <p className="text-xs font-extralight">No Items</p>
+            <p>$0.00</p>
+          </div>
+        </Link>
+      </div>
     </header>
   );
 }
