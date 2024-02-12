9a29a2a2b223dcaa950a03504191ab0e0d604d69
Stephen <79639335+stephen-gift@users.noreply.github.com>
2024-02-12 16:01:00 +0100

    GridOption

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,66 @@
+import GridOption from "@/components/GridOption";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <main className="">
+      <div>
+        <GridOption
+          title="Sweet gifts for less"
+          image="https://links.papareact.com/1dy"
+          className="bg-pink-200 h-full md:h-32"
+        />
+        <GridOption
+          title="Shop Wardrobe"
+          image="https://links.papareact.com/8ko"
+          className="bg-blue-100 col-span-2 row-span-2"
+        />
+        <GridOption
+          title="Shop Home"
+          image="https://links.papareact.com/szu"
+          className="bg-pink-200 row-span-2"
+        />
+        <GridOption
+          title="Shop Electronics"
+          image="https://links.papareact.com/n7r"
+          className="bg-yellow-100 h-64"
+        />
+        <GridOption
+          title="Shop Toys"
+          image="https://links.papareact.com/pj2"
+          className="bg-green-100 h-64 col-span-2"
+        />
+        <GridOption
+          title="All you need for Match Day"
+          image="https://links.papareact.com/m8e"
+          className="bg-red-100 col-span-2 row-span-2"
+        />
+        <GridOption
+          title="Shop Gadgets"
+          image="https://links.papareact.com/gs1"
+          className="bg-orange-100 h-64"
+        />
+        <GridOption
+          title="Shop Beauty"
+          image="https://links.papareact.com/4y0"
+          className="bg-orange-100 h-64"
+        />
+        <GridOption
+          title="Shop Sports"
+          image="https://links.papareact.com/sq2"
+          className="bg-orange-100 h-64"
+        />
+        <GridOption
+          title="Enjoy Free Shipping"
+          image="https://links.papareact.com/9fh"
+          className="bg-orange-100 h-64"
+        />
+        <GridOption
+          title="Flash Deals"
+          image="https://links.papareact.com/qx7"
+          className="bg-orange-200 h-64 col-span-2"
+        />
+      </div>
       <h1>Lets build a Walmart Clone</h1>
     </main>
   );
---
+++ components/GridOption.tsx
@@ -0,0 +1,35 @@
+import { cn } from "@/lib/utils";
+import Image from "next/image";
+import Link from "next/link";
+
+type Props = {
+  title: string;
+  className?: string;
+  image?: string;
+};
+function GridOption({ title, className, image }: Props) {
+  return (
+    <Link
+      href={{
+        pathname: "/",
+        query: {
+          q: title,
+        },
+      }}
+      className={cn("gridOption relative", className)}
+    >
+      <h2>{title}</h2>
+
+      {image && (
+        <Image
+          src={image}
+          alt={title}
+          layout="fill"
+          className="object-cover opacity-20 rounded-md"
+        />
+      )}
+    </Link>
+  );
+}
+
+export default GridOption;
