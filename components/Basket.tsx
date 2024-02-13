c6b566d787f974386b795daf88d340b264b1f05e
Stephen <79639335+stephen-gift@users.noreply.github.com>
2024-02-13 14:57:05 +0100

    grouped by sku

--- components/Basket.tsx
+++ components/Basket.tsx
@@ -1,13 +1,17 @@
 "use client";
 
 import { getCartTotal } from "@/lib/getCartTotal";
+import { groupBySKU } from "@/lib/groupBySKU";
 import { useCartStore } from "@/store";
 import React from "react";
 
 function Basket() {
   const cart = useCartStore((state) => state.cart);
+  const grouped = groupBySKU(cart);
   const basketTotal = getCartTotal(cart);
 
+  console.log(cart);
+  console.log(grouped);
   return <div>Basket</div>;
 }
 
