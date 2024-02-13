b993d78096f471e3b79254687ecc0299ede864b5
Stephen <79639335+stephen-gift@users.noreply.github.com>
2024-02-13 13:31:03 +0100

    update product page

--- app/product/page.tsx
+++ app/product/page.tsx
@@ -16,8 +16,8 @@ import {
   TableHead,
   TableHeader,
   TableRow,
-} from "@/components/ui/table"
-
+} from "@/components/ui/table";
+import { Badge } from "@/components/ui/badge";
 
 type Props = {
   searchParams: {
@@ -31,7 +31,7 @@ async function ProductPage({ searchParams: { url } }: Props) {
 
   if (!product) return notFound();
   return (
-    <div>
+    <div className="p-4 lg:p-10 flex flex-col lg:flex-row w-full">
       <div className="hidden lg:inline space-y-4 ">
         {product.images.map((image, i) => (
           <Image
@@ -49,11 +49,11 @@ async function ProductPage({ searchParams: { url } }: Props) {
         opts={{
           loop: true,
         }}
-        className="w-3/5 lg:mb-0 lg:w-full self-start flex items-center max-w-xl mx-auto lg:mx-20"
+        className="w-3/5 mb-10 lg:mb-0 lg:w-full self-center flex items-center max-w-xl mx-auto lg:mx-20"
       >
         <CarouselContent>
-          {product.images.map((image, i) => (
-            <CarouselItem key={image}>
+          {product.images?.map((image, i) => (
+            <CarouselItem key={i}>
               <div className="p-1">
                 <div className="flex aspect-square items-center justify-center p-2 relative">
                   <Image
@@ -70,6 +70,63 @@ async function ProductPage({ searchParams: { url } }: Props) {
         <CarouselPrevious />
         <CarouselNext />
       </Carousel>
+
+      <div className="flex-1 border rounded-md w-full p-5 space-y-5">
+        <h1 className="text-3xl font-bold">{product.title}</h1>
+
+        <div className="space-x-2">
+          {product?.breadcrumbs?.map((breadcrumb, i) => (
+            <Badge
+              key={breadcrumb + 1}
+              className={breadcrumb}
+              variant={"outline"}
+            >
+              {breadcrumb}
+            </Badge>
+          ))}
+        </div>
+        <div
+          dangerouslySetInnerHTML={{ __html: product.description }}
+          className="py-5"
+        />
+
+        <p className="text-yellow-500 text-sm">
+          {product.rating?.rating} *
+          <span className="text-gray-400 ml-2">
+            ({product.rating.count} reviews)
+          </span>
+        </p>
+
+        <p className="text-2xl font-bold mt-2">
+          {product?.currency} {product?.price}
+        </p>
+
+        {/* Add to Cart Button */}
+        {/* <AddToCart
+          product={product}
+        /> */}
+
+        <hr />
+
+        <h3 className="font-bold text-xl pt-10">Specifications</h3>
+
+        <Table>
+          <TableHeader>
+            <TableRow>
+              <TableHead>Specification</TableHead>
+              <TableHead>Value</TableHead>
+            </TableRow>
+          </TableHeader>
+          <TableBody>
+            {product?.specifications?.map((spec) => (
+              <TableRow key={spec.key}>
+                <TableCell className="font-bold">{spec.key}</TableCell>
+                <TableCell>{spec.value}</TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </div>
     </div>
   );
 }
