49a3937dd8f97b38022d5695836f27ed3c2fdacd
Stephen <79639335+stephen-gift@users.noreply.github.com>
2024-02-13 13:56:22 +0100

    AddToCart func

---
+++ components/ui/AddToCart.tsx
@@ -0,0 +1,10 @@
+import { Product } from '@/typings/productTypings'
+import React from 'react'
+
+function AddToCart({product}:{product:Product}) {
+  return (
+    <div>AddToCart</div>
+  )
+}
+
+export default AddToCart
